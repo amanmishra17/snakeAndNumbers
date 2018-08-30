78b43ef065fa87f6de7f950ce69577ec24ad8a28
murligoel <murli.goel1999@gmail.com>
2018-08-31 01:43:15 +0530

    added bugs to increase the snake length

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 var mySnake;
 var obstacles = [];
 var score;
+var food = [];
 
 function startGame() {
      myGameArea.start();
-    mySnake = new component(30,90,"yellow",125,430); 
+    mySnake = new component(30,90,"./img/snake.png",125,430,"image"); 
     score = new component("30px", "Consolas", "white", 250, 40, "text");
 }
 
@@ -52,7 +53,7 @@ function component(width,height,color,x,y,type){
     this.y=y;
     
     this.update = function(){
-    ctx = myGameArea.context; 
+    var ctx = myGameArea.context; 
     if (type == "image") {
       ctx.drawImage(this.image, 
         this.x, 
@@ -80,9 +81,12 @@ function component(width,height,color,x,y,type){
          else if(this.x<0)
              {this.x+=1;}
          
-         else if(this.y<0)
+         else if(this.y<250)
              {this.y+=1;}
         
+         else if(this.height+this.y > myGameArea.canvas.height +100)
+            { this.y-=1;}
+         
          else
             { this.x += 3*this.speedX;
               this.y += 3*this.speedY;
@@ -110,45 +114,75 @@ function component(width,height,color,x,y,type){
 }
     
 function updateGameArea(){ 
-    var x;
+    var i,j;
     
      for (i = 0; i < obstacles.length; i += 1) {
         if (mySnake.collision(obstacles[i])) {
-            mySnake.height = mySnake.height - 10;
-            obstacles[i].width = 0;
-            obstacles[i].height = -200;
-            myGameArea.scoring+=10;
+            mySnake.height = mySnake.height - 20;
+            obstacles[i].width = -5000;
+            obstacles[i].height = -5000;
+            
             }
             
-            if(mySnake.height == 0)
+            if(mySnake.height <= 0)
             myGameArea.stop();
+    }
+    
+    for (j = 0; j < food.length; j += 1) {
+        if (mySnake.collision(food[j])) {
+            mySnake.height = mySnake.height + 10;
+            food[j].width = -5000;
+            food[j].height = -5000;
+            myGameArea.scoring+=10;
+            }
     }
     
     
      myGameArea.clear();
     myGameArea.frame += 1;
     if (everyinterval(200)) {
-        x = myGameArea.canvas.width;
+    
         obstacles.push(new component(40, 40, "./img/1.png", Math.floor(Math.random()*50)+10, -10, "image"));
-        obstacles.push(new component(40, 40, "./img/2.png", Math.floor(Math.random()*400)+50, -10,"image"));
-        console.log(obstacles)
+        obstacles.push(new component(40, 40, "./img/2.png", Math.floor(Math.random()*100)+90, -10,"image"));
         
     }
     
     if ( everyinterval(150)) {
-        x = myGameArea.canvas.width;
-     obstacles.push(new component(40,40, "./img/3.png", Math.floor(Math.random()*250)+90, -10,"image"));
         
-        obstacles.push(new component(40,40, "./img/4.png", Math.floor(Math.random()*450)+130, -10,"image"));
+     obstacles.push(new component(40,40, "./img/3.png", Math.floor(Math.random()*100)+200, -10,"image"));
+        
+        obstacles.push(new component(40,40, "./img/4.png", Math.floor(Math.random()*200)+290, -10,"image"));
+          
+    }
+    
+    if (everyinterval(400)) {
+    
+        food.push(new component(40, 40, "./img/cockroaches.png", Math.floor(Math.random()*50)+90, -10, "image"));
+        food.push(new component(40, 40, "./img/frog.png", Math.floor(Math.random()*125)+180, -10, "image"));
+        
+    }
+    
+    if ( everyinterval(500)) {
+        
+     food.push(new component(40,40, "./img/grasshopper.png", Math.floor(Math.random()*250)+300, -10,"image"));
+        
           
     }
     for (i = 0; i < obstacles.length; i += 1) {
-          if(myGameArea.frame>=0 && myGameArea.frame <=2000)
+          if(myGameArea.frame>=0 && myGameArea.frame <= 4000)
            obstacles[i].y += 1;
-          if(myGameArea.frame>2000)
-           obstacles[i].y += 2;
+          if(myGameArea.frame>4000)
+              obstacles[i].y += 2;
            obstacles[i].update();
     }
+    for (j = 0; j < food.length; j += 1) {
+          if(myGameArea.frame>=0 && myGameArea.frame <=4000)
+           food[j].y += 1.5;
+          if(myGameArea.frame>=4000)
+           food[j].y += 2.5;
+           food[j].update();
+    }
+    
     score.text="My Score: " + myGameArea.scoring;
     score.update();
     
