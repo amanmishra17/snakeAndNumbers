056dd1027694d9b4f3d5a262ad971b75aa2500ec
murligoel <murli.goel1999@gmail.com>
2018-09-01 14:45:40 +0530

    project completed

--- game.js
+++ game.js
@@ -2,22 +2,25 @@ var mySnake;
 var obstacles = [];
 var score;
 var food = [];
-var gameOver;
 var food_sound = new Audio("bite.wav");
 var obstacles_sound = new Audio("obs_bite.wav");
+var count=0;
+
 
 function startGame() {
+    if(count==0){
      myGameArea.start();
-    mySnake = new component(40,150,"./img/s5.png",125,430,"image"); 
+    mySnake = new component(35,150,"./img/s6.png",125,430,"image"); 
     score = new component("30px", "Consolas", "white", 250, 40, "text");
-    gameOver = new component("30px", "Consolas", "white", 150, 320, "text");
+    count++;
+}
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 500;
-        this.canvas.height = 640;
+        this.canvas.width = 470;
+        this.canvas.height = 645;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -25,11 +28,9 @@ var myGameArea = {
         this.scoring=0;
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
-            
         })
         window.addEventListener('keyup', function (e) {
             myGameArea.key = false;
-            
         })
         
     },
@@ -39,9 +40,8 @@ var myGameArea = {
     },
     
     stop : function() {
-        gameOver.text="GAME OVER";
-                gameOver.update();      
         
+        document.getElementById("myimage").style.display = 'block';
         clearInterval(this.interval);
     }    
 }
@@ -88,11 +88,11 @@ function component(width,height,color,x,y,type){
          else if(this.x<0)
              {this.x+=1;}
          
-         else if(this.y<250)
-             {this.y+=1;}
+         else if(this.y<150)
+             {this.y+=50;}
         
-         else if(this.height+this.y > myGameArea.canvas.height +100)
-            { this.y-=1;}
+         else if(this.height+this.y > myGameArea.canvas.height+100)
+            { this.y-=200;}
          
          else
             { this.x += 3*this.speedX;
@@ -131,7 +131,7 @@ function updateGameArea(){
             obstacles[i].height = -1000;
             }
             
-            if(mySnake.height <= 0){
+            if(mySnake.height <= 15){
                 mySnake.height=0;      
                 myGameArea.stop();
             }
@@ -150,63 +150,80 @@ function updateGameArea(){
     
      myGameArea.clear();
     myGameArea.frame += 1;
-    if (everyinterval(200)) {
+    if (everyinterval(100)) {
     
-        obstacles.push(new component(40, 40, "./img/3.png", Math.floor(Math.random()*50)+10, -10, "image"));
+        obstacles.push(new component(20, 20, "./img/1.png", Math.floor(Math.random()*50)+10, -10, "image"));
         obstacles.push(new component(28, 28, "./img/2.png", Math.floor(Math.random()*100)+90, -10,"image"));
         
     }
     
-    if ( everyinterval(150)) {
+    if ( everyinterval(125)) {
         
-     obstacles.push(new component(33,33, "./img/4.png", Math.floor(Math.random()*100)+200, -10,"image"));
-        
-        obstacles.push(new component(20,20, "./img/1.png", Math.floor(Math.random()*200)+290, -10,"image"));
+      obstacles.push(new component(40,40, "./img/4.png", Math.floor(Math.random()*100)+200, -10,"image"));
+            
+      obstacles.push(new component(33,33, "./img/3.png", Math.floor(Math.random()*200)+250, -10,"image"));
           
     }
     
-    if (everyinterval(400)) {
+    if (everyinterval(250)) {
     
         food.push(new component(40, 40, "./img/cockroaches.png", Math.floor(Math.random()*50)+90, -10, "image"));
-        food.push(new component(40, 40, "./img/frog.png", Math.floor(Math.random()*125)+180, -10, "image"));
+        food.push(new component(40, 40, "./img/frog.png", Math.floor(Math.random()*125)+200, -10, "image"));
         
     }
-    if ( everyinterval(500)) {
+    if ( everyinterval(300)) {
         
-     food.push(new component(40,40, "./img/grasshopper.png", Math.floor(Math.random()*250)+300, -10,"image"));
+     food.push(new component(40,40, "./img/grasshopper.png", Math.floor(Math.random()*250)+230, -10,"image"));
         
           
     }
     
-    if(myGameArea.frame>2000)
-        {
-            if (everyinterval(170)) {
+    if(myGameArea.frame>1500 )    {
+            if (everyinterval(280)) {
     
-        obstacles.push(new component(40, 40, "./img/4.png", Math.floor(Math.random()*50)+10, -10, "image"));
-        obstacles.push(new component(28, 28, "./img/2.png", Math.floor(Math.random()*100)+90, -10,"image"));
+        obstacles.push(new component(40, 40, "./img/4.png", Math.floor(Math.random()*50)+30, -10, "image"));
+        obstacles.push(new component(33, 33, "./img/3.png", Math.floor(Math.random()*100)+90, -10,"image"));
         
     }
+      }
     
+         if(myGameArea.frame>2500 ) {
         
-            
-        }
+              if (everyinterval(370)) {
+    
+        obstacles.push(new component(40, 40, "./img/4.png", Math.floor(Math.random()*50)+30, -10, "image"));
+        obstacles.push(new component(28, 28, "./img/2.png", Math.floor(Math.random()*100)+90, -10,"image"));
+        obstacles.push(new component(45, 45, "./img/5.png", Math.floor(Math.random()*450)+90, -10,"image"));
+    }
+                        
+         }
     
     for (i = 0; i < obstacles.length; i += 1) {
-          if(myGameArea.frame>=0 && myGameArea.frame <= 2000)
-           obstacles[i].y += 1;
-          if(myGameArea.frame>2000)
-              obstacles[i].y += 2;
-           obstacles[i].update();
+          if(myGameArea.frame>=0 && myGameArea.frame <= 1000)
+           obstacles[i].y += 2;
+          if(myGameArea.frame>1000 && myGameArea.frame<=2000)
+              obstacles[i].y += 2.5;
+           if(myGameArea.frame>2000 && myGameArea.frame<=3000)
+              obstacles[i].y += 3.0;
+           if(myGameArea.frame>3000 && myGameArea.frame<=5000)
+               obstacles[i].y+= 3.5;
+            if(myGameArea.frame>5000 && myGameArea.frame<=6000)
+              obstacles[i].y += 4.0;
+            if(myGameArea.frame>6000)
+              obstacles[i].y += 8;
+            obstacles[i].update();
     }
     for (j = 0; j < food.length; j += 1) {
           if(myGameArea.frame>=0 && myGameArea.frame <=2000)
-           food[j].y += 1.5;
-          if(myGameArea.frame>=2000)
-           food[j].y += 2.5;
+           food[j].y += 2.8;
+          if(myGameArea.frame>2000 && myGameArea.frame<=5000)
+           food[j].y += 4;
+          if(myGameArea.frame>5000)
+              food[j].y+=6.5;
            food[j].update();
     }
     
-    score.text="My Score: " + myGameArea.scoring;
+    score.text = "Score: " + myGameArea.scoring;
     score.update();
     
     mySnake.speedX=0;
@@ -225,7 +242,3 @@ function everyinterval(n) {
     if (myGameArea.frame % n == 0) {return true;}
     return false;
 }
-
-
-
-    
